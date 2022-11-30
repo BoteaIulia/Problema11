81916feb3b31145418313b2bdc9d93d42c8c3c9b
botea <botea@IULIA>
2022-11-30 13:20:52 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+public static class Globals
+{
+	internal static void Main()
+	{
+		Console.Write("Afisati in ordine inversa cifrele lui N");
+		Console.Write("\n");
+
+		int n;
+		Console.Write("N = ");
+		n = Convert.ToInt32(Console.ReadLine());
+
+		int ogl = 0;
+		while (n != 0)
+		{
+			ogl = 10 * ogl + n % 10;
+			n /= 10;
+		}
+		Console.Write("Oglinditul lui N = ");
+		Console.Write(ogl);
+		Console.Write("\n");
+	}
+}
